e020e2741d62a188196e89fad7a4fd7730f4e522
JonM <jcmeisner7@gmail.com>
2022-04-27 11:20:34 -0700

    Worked on Filter Buttons and Initial Searchbar

--- src/containers/BinderContainer.tsx
+++ src/containers/BinderContainer.tsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import { makeStyles } from '@mui/styles';
+import { styled } from '@mui/material/styles';
+import { TextField, InputLabel } from '@mui/material';
+import SearchIcon from '@mui/icons-material/Search';
 
 const styles = makeStyles((theme) => ({
     binderOuter: {
@@ -13,15 +16,17 @@ const styles = makeStyles((theme) => ({
         display: 'flex',
         justifyContent: 'space-evenly',
         position: 'absolute',
-        top: '15vh',
-        width: '50vw',
-        height: '70vh',
+        top: '7.5vh',
+        width: '70vw',
+        height: '85vh',
         border: 'solid black 2px',
         backgroundColor: '#333844',
         color: 'white',
+        borderRadius: '5px',
     },
-    binderSection: {
-        border: 'solid white 2px',
+    leftBinderSection: {
+        borderRight: 'solid white 2px',
+        "-webkit-scrollbar": {display: "none"},
         display: 'grid',
         gap: '5%',
         gridTemplateColumns: 'repeat(3, auto)',
@@ -30,20 +35,98 @@ const styles = makeStyles((theme) => ({
         justifyItems: 'center',
         alignContent: 'space-evenly',
         alignItems: 'center',
+        paddingLeft: '2vw',
+        paddingRight: '2vw',
+        borderTopLeftRadius: '5px',
+        borderBottomLeftRadius: '5px',
+    },
+    rightBinderSection: {
+        borderLeft: 'solid white 2px',
+        "-webkit-scrollbar": {display: "none"},
+        display: 'grid',
+        gap: '5%',
+        gridTemplateColumns: 'repeat(3, auto)',
+        width: '100%',
+        justifyContent: 'space-evenly',
+        justifyItems: 'center',
+        alignContent: 'space-evenly',
+        alignItems: 'center',
+        paddingLeft: '2vw',
+        paddingRight: '2vw',
+        borderTopRightRadius: '5px',
+        borderBottomRightRadius: '5px',
     },
     cardSlot: {
-        height: '14vh',
-        width: '10vh',
-        border: 'solid red 2px',
+        height: '21vh',
+        width: '15vh',
+        borderLeft: 'solid red 2px',
+        borderBottom: 'solid red 2px',
+        borderRight: 'solid red 2px',
+    },
+    rarityButtons: {
+        position: 'absolute',
+        left: '27vw',
+        top: '93vh',
+        display: 'flex',
+        justifyContent: 'space-evenly',
+        border: 'solid black 2px',
+    },
+    rarityButton: {
+
+    },
+    searchBarContainer: {
+        backgroundColor: '#333844',
+        height: '5.5vh',
+        postition: 'absolute',
+        left: '77vw',
+        top: '93vh',
+        border: 'solid black 2px',
+        borderBottomRightRadius: '5px',
+        borderBottomLeftRadius: '5px',
+        color: 'white',
+        '& .MuiOutlinedInput-root': {
+            color: 'white',
+            '& fieldset': {
+              borderColor: 'white',
+            },
+          },
     }
 }))
+
+const CssTextField = styled(TextField)({
+    '& label.Mui-focused': {
+      color: 'white',
+    },
+    // '& label': {
+    //     color: 'white',
+    // },
+    '& .MuiInputLabel-root': {
+        color: 'white',
+        paddingBottom: '5vh',
+    },
+    '& .MuiInput-underline:after': {
+      borderBottomColor: 'green',
+    },
+    '& .MuiOutlinedInput-root': {
+        height: '3vh',
+      '& fieldset': {
+        borderColor: 'darkgrey',
+      },
+      '&:hover fieldset': {
+        borderColor: 'grey',
+      },
+      '&.Mui-focused fieldset': {
+        borderColor: 'white',
+      },
+    },
+  });
 
 const BinderContainer = () => {
     const classes = styles();
     return (
         <div className={classes.binderOuter}>
             <div className={classes.binderMain}>
-                <div className={classes.binderSection}>
+                <div className={classes.leftBinderSection}>
                     <div className={classes.cardSlot}></div>
                     <div className={classes.cardSlot}></div>
                     <div className={classes.cardSlot}></div>
@@ -54,7 +137,7 @@ const BinderContainer = () => {
                     <div className={classes.cardSlot}></div>
                     <div className={classes.cardSlot}></div>
                 </div>
-                <div className={classes.binderSection}>
+                <div className={classes.rightBinderSection}>
                     <div className={classes.cardSlot}></div>
                     <div className={classes.cardSlot}></div>
                     <div className={classes.cardSlot}></div>
@@ -65,6 +148,16 @@ const BinderContainer = () => {
                     <div className={classes.cardSlot}></div>
                     <div className={classes.cardSlot}></div>
                 </div>
+            </div>
+            <div className={classes.rarityButtons}>
+                <button>HOLO</button>
+                <button>EPIC</button>
+                <button>LEGENDARY</button>
+            </div>
+            <div className={classes.searchBarContainer}>
+                <InputLabel htmlFor='card_search'>
+                    <CssTextField  id='card_search' label="Search" variant="outlined"/>   
+                </InputLabel>
             </div>
         </div>
     )
